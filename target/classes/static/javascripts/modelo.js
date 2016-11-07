dfdbb30fbc6245305885a09a8db4336ccf893a17
Anderson Salles <agsalles@gmail.com>
2016-11-07 17:52:55 -0300

    16.3.Configurando máscara de CPFCNPJ com jQuery

---
+++ target/classes/static/javascripts/modelo.js
@@ -0,0 +1,21 @@
+var Brewer = Brewer || {}
+
+Brewer.MascaraCpfCnpj = (function(){
+	
+	function MascaraCpfCnpj(){
+		
+	}
+	
+	MarcaraCpfCnpj.prototype.iniciar = function(){
+		
+	}
+	
+	return MarcaraCpfCnpj;
+	
+}());
+
+$(function(){
+	var marcaraCpfCnpj = new Brewer.MarcaraCpfCnpj();
+	marcaraCpfCnpj.iniciar();
+	
+});
