1c16bd27dfaeb043ff3dc778ebe29f5eda0b8d19
Anderson Salles <agsalles@gmail.com>
2016-11-09 11:04:10 -0300

    16.6. Aplicando migração estado e cidade

---
+++ src/main/java/com/algaworks/brewer/model/Estado.java
@@ -0,0 +1,73 @@
+package com.algaworks.brewer.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "estado")
+public class Estado implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	private Long Codigo;
+	private String nome;
+	private String sigla;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	public Long getCodigo() {
+		return Codigo;
+	}
+	public void setCodigo(Long codigo) {
+		Codigo = codigo;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getSigla() {
+		return sigla;
+	}
+	public void setSigla(String sigla) {
+		this.sigla = sigla;
+	}
+	
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((Codigo == null) ? 0 : Codigo.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Estado other = (Estado) obj;
+		if (Codigo == null) {
+			if (other.Codigo != null)
+				return false;
+		} else if (!Codigo.equals(other.Codigo))
+			return false;
+		return true;
+	}
+
+	
+	
+	
+	
+	
+
+}
